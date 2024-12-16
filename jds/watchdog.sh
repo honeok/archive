3867f017ba8761d9002f81f54513d48be28c2f17
honeok <yihaohey@gmail.com>
2024-12-16 20:23:03 +0800

    update

--- jds/watchdog.sh
+++ jds/watchdog.sh
@@ -8,16 +8,30 @@
 # ./watchdog.sh 1/2/3/4/5...       # Starts the game server(s) corresponding to the given IDs.
 #
 # Copyright (C) 2024 honeok <honeok@duck.com>
-# Blog: www.honeok.com
-# https://github.com/honeok/archive/blob/master/jds/watchdog.sh
+
+set \
+    -o errexit \
+    -o nounset
+
+version='v0.0.2 (2024.12.16)'
+server_number=""
+open_server_time=""
 
 game1="10.46.99.216"
 game2="127.0.0.1"
 
+export DEBIAN_FRONTEND=noninteractive
+
+# 操作系统和权限校验
 os_name=$(grep ^ID= /etc/*release | awk -F'=' '{print $2}' | sed 's/"//g')
 [[ "$os_name" != "debian" && "$os_name" != "ubuntu" && "$os_name" != "centos" && "$os_name" != "rocky" && "$os_name" != "alma" ]] && exit 0
+[ "$(id -u)" -ne "0" ] && exit 1
 
-[ "$(id -u)" -ne "0" ] && exit 0
+# 检查密码文件
+# echo "xxxxxxxxxxxx" > /root/password.txt && chmod 600 /root/password.txt 只有root用户可以读取该文件
+[ -f /root/password.txt ] && [ -s /root/password.txt ] || exit 1
+server_password=$(cat /root/password.txt)
+[ -n "$server_password" ] || exit 1
 
 if [ "$(cd -P -- "$(dirname -- "$0")" && pwd -P)" != "/root" ]; then
     cd /root >/dev/null 2>&1
@@ -43,13 +57,21 @@ else
     server_number=$1
 fi
 
+# 根据区服编号匹配服务器IP
+if (( server_number >= 1 && server_number <= 5 )); then
+    server_ip=${game1}
+elif (( server_number >= 6 && server_number <= 10 )); then
+    server_ip=${game2}
+else
+    _exit
+fi
+
 # 开服所需时间相关
-open_server_time=""
 taobao_timeapi=$(date -d @$(($(curl -sL https://acs.m.taobao.com/gw/mtop.common.getTimestamp/ | awk -F'"t":"' '{print $2}' | cut -d '"' -f1) / 1000)) +"%Y-%m-%dT%H:00:00")
 suning_timeapi=$(date -d @$(($(curl -sL https://f.m.suning.com/api/ct.do | awk -F'"currentTime": ' '{print $2}' | cut -d ',' -f1) / 1000)) +"%Y-%m-%dT%H:00:00")
 
-for api in "$taobao_timeapi" "$suning_timeapi"; do
-    open_server_time=$api  # 将当前API返回的时间赋值给open_server_time
+for timeapi in "$taobao_timeapi" "$suning_timeapi"; do
+    open_server_time=$timeapi  # 将当前API返回的时间赋值给open_server_time
 
     # 检查时间格式是否有效
     if [[ -n "$open_server_time" && "$open_server_time" =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:00:00$ ]]; then
@@ -67,23 +89,11 @@ if [ -z $china_time ]; then
     china_time=$(date -d @$(($(curl -sL https://f.m.suning.com/api/ct.do | awk -F'"currentTime": ' '{print $2}' | cut -d ',' -f1) / 1000)) +"%Y-%m-%dT%H:%M:%S")
 fi
 
-# echo "xxxxxxxxxxxx" > /root/password.txt && chmod 600 /root/password.txt 只有root用户可以读取该文件
-[ -f /root/password.txt ] && [ -s /root/password.txt ] && server_password=$(cat /root/password.txt) || exit 1
-
-# 根据区服编号匹配服务器IP
-if (( server_number >= 1 && server_number <= 5 )); then
-    server_ip=${game1}
-elif (( server_number >= 6 && server_number <= 10 )); then
-    server_ip=${game2}
-else
-    _exit
-fi
-
-# API 回调
+# api回调
 send_message() {
     local action="$1"
-    local country=$(curl -s ipinfo.io/country || echo "unknown")
-    local os_info=$(grep '^PRETTY_NAME=' /etc/os-release | cut -d '"' -f 2 | sed 's/ (.*)//')
+    local country=$(curl -s https://ipinfo.io/country || echo "unknown")
+    local os_info=$(grep ^ID= /etc/*release | awk -F'=' '{print $2}' | sed 's/"//g')
     local cpu_arch=$(uname -m)
 
     curl -s -X POST "https://api.honeok.com/api/log" \
@@ -93,17 +103,13 @@ send_message() {
 
 if ! command -v sshpass >/dev/null 2>&1 && type -P sshpass >/dev/null 2>&1; then
     if command -v dnf >/dev/null 2>&1; then
-        if ! rpm -q epel-release >/dev/null 2>&1; then
-            dnf install epel-release -y
-        fi
+        [[ ! $(rpm -q epel-release) ]] && dnf install epel-release -y
         dnf update -y && dnf install sshpass -y
     elif command -v yum >/dev/null 2>&1; then
-        if ! rpm -q epel-release >/dev/null 2>&1; then
-            yum install epel-release -y
-        fi
+        [[ ! $(rpm -q epel-release) ]] && yum install epel-release -y
         yum update -y && yum install sshpass -y
-    elif command -v apt-get >/dev/null 2>&1; then
-        apt-get update -y && apt-get install sshpass -y
+    elif command -v apt >/dev/null 2>&1; then
+        apt update -y && apt install sshpass -y
     else
         _exit
     fi
@@ -113,11 +119,11 @@ fi
 remote_command="\
 # 进入游戏目录，修改开服时间
 cd /data/server${server_number}/game || exit 1 && \
-[ -f lua/config/profile.lua ] || exit 1 && \
-sed -i '/^\s*local open_server_time\s*=/s|\"[^\"]*\"|\"'"$openserver_time"'\"|' lua/config/profile.lua || exit 1 && \
-grep -q '^\s*local open_server_time\s*=\s*\"'"$openserver_time"'\"' lua/config/profile.lua || exit 1 && \
+[ -f lua/config/open_time.lua ] || exit 1 && \
+sed -i '/^\s*open_server_time\s*=/s|\"[^\"]*\"|\"'"$openserver_time"'\"|' lua/config/open_time.lua || exit 1 && \
+grep -q "^\s*open_server_time\s*=\s*\"$openserver_time\"" lua/config/open_time.lua || exit 1 && \
 # 检查文件是否在过去1分钟内被修改
-if ! find lua/config/profile.lua -mmin -1 >/dev/null 2>&1; then exit 1; fi && \
+if ! find lua/config/open_time.lua -mmin -1 >/dev/null 2>&1; then exit 1; fi && \
 ./server.sh reload || exit 1 && \
 
 # 进入登录目录，修改白名单
