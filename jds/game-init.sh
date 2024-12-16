785251f3624855271c0dd4e55a8dfaf96f294acd
honeok <yihaohey@gmail.com>
2024-12-16 17:39:52 +0800

    update

---
+++ jds/game-init.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#
+# Description: Game configuration initialization file distribution
+#
+# Copyright (C) 2024 JDSGame <auther: honeok@duck.com>
+# Blog: www.honeok.com
+# https://github.com/honeok/archive/blob/master/jds/game-init.sh
+
