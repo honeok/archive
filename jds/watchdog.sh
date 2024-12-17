51f91bb3a309d3edd4fd5a9974592c1d59c1fc00
honeok <yihaohey@gmail.com>
2024-12-17 15:40:56 +0800

    update

--- jds/watchdog.sh
+++ jds/watchdog.sh
@@ -9,11 +9,18 @@
 #
 # Copyright (C) 2024 honeok <honeok@duck.com>
 
-set \
-    -o errexit \
-    -o nounset
+version='v0.0.2 (2024.12.17)'
 
-version='v0.0.2 (2024.12.16)'
+# 信号捕捉相关
+trap _exit SIGINT SIGQUIT SIGTERM SIGHUP
+set -o errexit
+
+_exit() {
+    [ -f "$watchdog_pid" ] && rm -f "$watchdog_pid"
+    exit 0
+}
+
+# 预定义变量
 server_number=""
 open_server_time=""
 
@@ -22,33 +29,44 @@ game2="127.0.0.1"
 
 export DEBIAN_FRONTEND=noninteractive
 
-# 操作系统和权限校验
+# 消息回调
+china_time=$(date -d @$(($(curl -sL https://acs.m.taobao.com/gw/mtop.common.getTimestamp/ | awk -F'"t":"' '{print $2}' | cut -d '"' -f1) / 1000)) +"%Y-%m-%dT%H:%M:%S")
+if [ -z $china_time ]; then
+    china_time=$(date -d @$(($(curl -sL https://f.m.suning.com/api/ct.do | awk -F'"currentTime": ' '{print $2}' | cut -d ',' -f1) / 1000)) +"%Y-%m-%dT%H:%M:%S")
+fi
+
+send_message() {
+    local action="$1"
+    local country=$(curl -sL https://ipinfo.io/country || echo "unknown")
+    local os_info=$(grep ^ID= /etc/*release | awk -F'=' '{print $2}' | sed 's/"//g')
+    local cpu_arch=$(uname -m)
+
+    curl -s -X POST "https://api.honeok.com/api/log" \
+        -H "Content-Type: application/json" \
+        -d "{\"action\":\"$action\",\"timestamp\":\"$china_time\",\"country\":\"$country\",\"os_info\":\"$os_info\",\"cpu_arch\":\"$cpu_arch\"}" >/dev/null 2>&1 &
+}
+
+# 服务器信息预检
 os_name=$(grep ^ID= /etc/*release | awk -F'=' '{print $2}' | sed 's/"//g')
 [[ "$os_name" != "debian" && "$os_name" != "ubuntu" && "$os_name" != "centos" && "$os_name" != "rocky" && "$os_name" != "alma" ]] && exit 0
 [ "$(id -u)" -ne "0" ] && exit 1
-
-# 检查密码文件
-# echo "xxxxxxxxxxxx" > /root/password.txt && chmod 600 /root/password.txt 只有root用户可以读取该文件
-[ -f /root/password.txt ] && [ -s /root/password.txt ] || exit 1
-server_password=$(cat /root/password.txt)
-[ -n "$server_password" ] || exit 1
 
 if [ "$(cd -P -- "$(dirname -- "$0")" && pwd -P)" != "/root" ]; then
     cd /root >/dev/null 2>&1
 fi
 
+# 获取服务器密码
+# echo "xxxxxxxxxxxx" > /root/password.txt && chmod 600 /root/password.txt
+[ -f /root/password.txt ] && [ -s /root/password.txt ] || exit 1
+server_password=$(cat /root/password.txt)
+[ -n "$server_password" ] || exit 1
+
+# 脚本执行pid写入 确保脚本执行进程唯一性
 watchdog_pid="/tmp/watchdog.pid"
 if [ -f "$watchdog_pid" ] && kill -0 $(cat "$watchdog_pid") 2>/dev/null; then
     exit 1
 fi
 echo $$ > "$watchdog_pid"
-
-trap _exit SIGINT SIGQUIT SIGTERM SIGHUP
-
-_exit() {
-    [ -f "$watchdog_pid" ] && rm -f "$watchdog_pid"
-    exit 0
-}
 
 # 脚本入参校验
 if [[ ${#} -ne 1 || ! $1 =~ ^[0-9]+$ ]]; then
@@ -57,7 +75,7 @@ else
     server_number=$1
 fi
 
-# 根据区服编号匹配服务器IP
+# 服务器IP匹配
 if (( server_number >= 1 && server_number <= 5 )); then
     server_ip=${game1}
 elif (( server_number >= 6 && server_number <= 10 )); then
@@ -70,8 +88,8 @@ fi
 taobao_timeapi=$(date -d @$(($(curl -sL https://acs.m.taobao.com/gw/mtop.common.getTimestamp/ | awk -F'"t":"' '{print $2}' | cut -d '"' -f1) / 1000)) +"%Y-%m-%dT%H:00:00")
 suning_timeapi=$(date -d @$(($(curl -sL https://f.m.suning.com/api/ct.do | awk -F'"currentTime": ' '{print $2}' | cut -d ',' -f1) / 1000)) +"%Y-%m-%dT%H:00:00")
 
-for timeapi in "$taobao_timeapi" "$suning_timeapi"; do
-    open_server_time=$timeapi  # 将当前API返回的时间赋值给open_server_time
+for api in "$taobao_timeapi" "$suning_timeapi"; do
+    open_server_time=$api  # 将当前API返回的时间赋值给open_server_time
 
     # 检查时间格式是否有效
     if [[ -n "$open_server_time" && "$open_server_time" =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:00:00$ ]]; then
@@ -84,23 +102,7 @@ if [[ -z "$open_server_time" || ! "$open_server_time" =~ ^[0-9]{4}-[0-9]{2}-[0-9
     open_server_time=$(date -u -d '+8 hours' +"%Y-%m-%dT%H:00:00")  # 使用当前时间并调整为北京时间 (UTC+8)，如果系统时间同步不可用，时间偏差通常不会太大
 fi
 
-china_time=$(date -d @$(($(curl -sL https://acs.m.taobao.com/gw/mtop.common.getTimestamp/ | awk -F'"t":"' '{print $2}' | cut -d '"' -f1) / 1000)) +"%Y-%m-%dT%H:%M:%S")
-if [ -z $china_time ]; then
-    china_time=$(date -d @$(($(curl -sL https://f.m.suning.com/api/ct.do | awk -F'"currentTime": ' '{print $2}' | cut -d ',' -f1) / 1000)) +"%Y-%m-%dT%H:%M:%S")
-fi
-
-# api回调
-send_message() {
-    local action="$1"
-    local country=$(curl -s https://ipinfo.io/country || echo "unknown")
-    local os_info=$(grep ^ID= /etc/*release | awk -F'=' '{print $2}' | sed 's/"//g')
-    local cpu_arch=$(uname -m)
-
-    curl -s -X POST "https://api.honeok.com/api/log" \
-        -H "Content-Type: application/json" \
-        -d "{\"action\":\"$action\",\"timestamp\":\"$china_time\",\"country\":\"$country\",\"os_info\":\"$os_info\",\"cpu_arch\":\"$cpu_arch\"}" >/dev/null 2>&1 &
-}
-
+# sshpass命令校验
 if ! command -v sshpass >/dev/null 2>&1 && type -P sshpass >/dev/null 2>&1; then
     if command -v dnf >/dev/null 2>&1; then
         [[ ! $(rpm -q epel-release) ]] && dnf install epel-release -y
@@ -115,7 +117,7 @@ if ! command -v sshpass >/dev/null 2>&1 && type -P sshpass >/dev/null 2>&1; then
     fi
 fi
 
-# 构建远程命令
+# 远程命令构建
 remote_command="\
 # 进入游戏目录，修改开服时间
 cd /data/server${server_number}/game || exit 1 && \
@@ -138,7 +140,7 @@ else \
 fi && \
 ./server.sh reload || exit 1"
 
-# 执行远程命令，三次重试机会
+# 执行远程命令
 for (( i=1; i<=3; i++ )); do
     # 执行远程命令，捕获错误信息
     output=$(sshpass -p "$server_password" ssh -o StrictHostKeyChecking=no root@$server_ip "$remote_command" 2>&1)
