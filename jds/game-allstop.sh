62ff26021a0b9fd4506b6e6af0426a1326ada7f1
honeok <yihaohey@gmail.com>
2024-12-30 13:58:47 +0800

    update

--- jds/game-allstop.sh
+++ jds/game-allstop.sh
@@ -10,18 +10,22 @@ yellow='\033[93m'
 red='\033[31m'
 green='\033[92m'
 white='\033[0m'
-_yellow() { echo -e ${yellow}$@${white}; }
-_red() { echo -e ${red}$@${white}; }
-_green() { echo -e ${green}$@${white}; }
+_yellow() { echo -e "${yellow}$*${white}"; }
+_red() { echo -e "${red}$*${white}"; }
+_green() { echo -e "${green}$*${white}"; }
 
 server_range=$(seq 1 5)
 
 clear
-cd /data/tool
+cd /data/tool || exit 1
 if pgrep -f processcontrol-allserver.sh >/dev/null 2>&1; then
     pkill -9 -f processcontrol-allserver.sh
-    [ -f "control.txt" ] && > control.txt
-    [ -f "dump.txt" ] && > dump.txt
+    if [ -f "control.txt" ]; then
+        > control.txt
+    fi
+    if [ -f "dump.txt" ]; then
+        > dump.txt
+    fi
     _green "processcontrol进程已终止文件已清空"
 else
     _red "processcontrol进程未运行无需终止"
@@ -31,10 +35,10 @@ cd /data/server/login/ && ./server.sh stop
 cd /data/server/gate/ && ./server.sh stop && sleep 60
 _green "login和gate服务器已停止"
 
-for i in $server_range; do
+for server_num in $server_range; do
     (
-        _yellow "正在处理server$i"
-        cd /data/server$i/game/
+        _yellow "正在处理server$server_num"
+        cd /data/server$server_num/game/
         ./server.sh flush
         sleep 60
         ./server.sh stop
