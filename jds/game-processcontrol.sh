f31ecacb87b7102aa4cffd8768eaa5e53cf80ccf
honeok <yihaohey@gmail.com>
2024-12-30 17:42:30 +0800

    ci: run ShellCheck to validate script syntax

--- jds/game-processcontrol.sh
+++ jds/game-processcontrol.sh
@@ -1,29 +1,31 @@
 #!/usr/bin/env bash
 #
-# Description: Resident daemon in the game server backend
+# Description: Resident daemon in the game server backend.
 #
-# Copyright (C) 2024 JDSGame <auther: honeok@duck.com>
-# Blog: www.honeok.com
-# https://github.com/honeok/archive/blob/master/jds/game-processcontrol.sh
+# Copyright (C) 2024 honeok <honeok@duck.com>
+#
+# https://github.com/honeok/archive/raw/master/jds/game-processcontrol.sh
 
-server_range=$(seq 1 5)   # Game服务器范围
-east8_time=$(date -d @$(($(curl -sL https://acs.m.taobao.com/gw/mtop.common.getTimestamp/ | awk -F'"t":"' '{print $2}' | cut -d '"' -f1) / 1000)) +"%Y-%m-%d %H:%M:%S")
-base_path="/data/server"
+server_range=$(seq 1 5) # 服务器范围
+log_bak="/data/logbak"
 app_name="p8_app_server"
+base_path="/data/server"
 
 # 日志备份目录校验
-[ ! -d /data/logback ] && mkdir -p /data/logback
+[ ! -d "$log_bak" ] && mkdir -p "$log_bak"
 
-# API回调
+# api回调
 send_message() {
     local action="$1"
-    local country=$(curl -s ipinfo.io/country || echo "unknown")
-    local os_info=$(grep '^PRETTY_NAME=' /etc/os-release | cut -d '"' -f 2 | sed 's/ (.*)//')
-    local cpu_arch=$(uname -m)
+    local country os_info cpu_arch
+
+    country=$(curl -fsL ipinfo.io/country || echo "unknown")
+    os_info=$(grep '^PRETTY_NAME=' /etc/os-release | cut -d '"' -f 2 | sed 's/ (.*)//')
+    cpu_arch=$(uname -m)
 
     curl -s -X POST "https://api.honeok.com/api/log" \
         -H "Content-Type: application/json" \
-        -d "{\"action\":\"$action\",\"timestamp\":\"${east8_time}\",\"country\":\"$country\",\"os_info\":\"$os_info\",\"cpu_arch\":\"$cpu_arch\"}" >/dev/null 2>&1 &
+        -d "{\"action\":\"$action\",\"timestamp\":\"$(date -u '+%Y-%m-%d %H:%M:%S' -d '+8 hours')\",\"country\":\"$country\",\"os_info\":\"$os_info\",\"cpu_arch\":\"$cpu_arch\"}" >/dev/null 2>&1 &
 }
 
 # 服务器运行状态校验
@@ -31,16 +33,18 @@ check_server() {
     local server_name=$1
     local server_dir=$2
 
-    ## 检查服务器进程是否在运行，如果没有则进行重启操作
-    if ! pgrep -f "$server_dir/$app_name" > /dev/null 2>&1; then
+    # 检查服务器进程是否在运行，如果没有则进行重启操作
+    if ! pgrep -f "$server_dir/$app_name" >/dev/null 2>&1; then
         # 服务没有运行进行重启操作
         cd "$server_dir" || return              # 进入服务器目录，如果失败则退出
-        [[ -f nohup.txt ]] && cp -f nohup.txt "/data/logback/nohup_${server_name}_$(date -u '+%Y%m%d%H%M%S' -d '+8 hours').txt" && rm -f nohup.txt
+        if [ -f nohup.txt ]; then
+            cp -f nohup.txt "${log_bak}/nohup_${server_name}_$(date -u '+%Y%m%d%H%M%S' -d '+8 hours').txt" && rm -f nohup.txt
+        fi
         ./server.sh start &
         send_message "[${server_name} Restart]" # 发送重启通知
-        echo "${east8_time} [ERROR] $server_name Restart" >> /data/tool/control.txt &
+        echo "$(date -u '+%Y-%m-%d %H:%M:%S' -d '+8 hours') [ERROR] $server_name Restart" >> /data/tool/control.txt &
     else
-        echo "${east8_time} [INFO] $server_name Running" >> /data/tool/control.txt &
+        echo "$(date -u '+%Y-%m-%d %H:%M:%S' -d '+8 hours') [INFO] $server_name Running" >> /data/tool/control.txt &
     fi
 }
 
