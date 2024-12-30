d0df4352ada8b130e7c13a858605ce4119e74081
honeok <yihaohey@gmail.com>
2024-12-30 17:40:03 +0800

    feat: enable automatic matching for game server

--- jds/game-allstop.sh
+++ jds/game-allstop.sh
@@ -3,40 +3,45 @@
 # Description: The game server is stopped in parallel.
 #
 # Copyright (C) 2024 honeok <honeok@duck.com>
-# Blog: www.honeok.com
-# https://github.com/honeok/archive/blob/master/jds/game-allstop.sh
+#
+# https://github.com/honeok/archive/raw/master/jds/game-allstop.sh
 
 yellow='\033[93m'
 red='\033[31m'
 green='\033[92m'
 white='\033[0m'
-_yellow() { echo -e ${yellow}$@${white}; }
-_red() { echo -e ${red}$@${white}; }
-_green() { echo -e ${green}$@${white}; }
+_yellow() { echo -e "${yellow}$*${white}"; }
+_red() { echo -e "${red}$*${white}"; }
+_green() { echo -e "${green}$*${white}"; }
 
-server_range=$(seq 1 5) # 服务器范围
+server_range=$(find /data/ -maxdepth 1 -type d -name "server*" | sed 's:.*/::' | grep -E '^server[0-9]+$' | sed 's/server//' | sort -n)
 
 clear
-cd /data/tool
+cd /data/tool || exit 1
 if pgrep -f processcontrol-allserver.sh >/dev/null 2>&1; then
     pkill -9 -f processcontrol-allserver.sh
-    [ -f "control.txt" ] && > control.txt
-    [ -f "dump.txt" ] && > dump.txt
+    [ -f "control.txt" ] && : > control.txt
+    [ -f "dump.txt" ] && : > dump.txt
     _green "processcontrol进程已终止文件已清空"
 else
     _red "processcontrol进程未运行无需终止"
 fi
 
-cd /data/server/login/ && ./server.sh stop
-cd /data/server/gate/ && ./server.sh stop && sleep 120s
-_green "login和gate服务器已停止"
+cd /data/server/login || exit 1
+./server.sh stop
+_green "login服务器已停止"
 
-for i in $server_range; do
+cd /data/server/gate || exit 1
+./server.sh stop
+sleep 60
+_green "gate服务器已停止"
+
+for server_num in $server_range; do
     (
-        _yellow "正在处理server$i"
-        cd /data/server$i/game/
+        cd "/data/server$server_num/game" || return
+        _yellow "正在处理server$server_num"
         ./server.sh flush
-        sleep 60s
+        sleep 60
         ./server.sh stop
     ) &
 done
