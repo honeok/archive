0fdb03c490bb12e23a363409335425a8d6b56f82
honeok <yihaohey@gmail.com>
2024-12-30 17:33:06 +0800

    refactor: improve logic and usability, final update and archived

--- nx-engine/aifengxing.sh
+++ nx-engine/aifengxing.sh
@@ -4,8 +4,8 @@
 #
 # Copyright (C) 2021 - 2022 nx-engine <yihao.he@nx-engine.com>
 #
-# Originally written on: 2022.01.14, archived after updates on: 2024.12.25
-# Company site: https://www.nx-engine.com
+# Archived after updates on: 2024.12.25
+# Website: https://www.nx-engine.com
 #
 # https://github.com/honeok/archive/raw/master/nx-engine/aifengxing.sh
 
@@ -20,14 +20,15 @@ yellow='\033[93m'
 red='\033[31m'
 green='\033[92m'
 white='\033[0m'
-_yellow() { echo -e ${yellow}$@${white}; }
-_red() { echo -e ${red}$@${white}; }
-_green() { echo -e ${green}$@${white}; }
+_yellow() { echo -e "${yellow}$*${white}"; }
+_red() { echo -e "${red}$*${white}"; }
+_green() { echo -e "${green}$*${white}"; }
+_err_msg() { echo -e "\033[41m\033[1m警告${white} $*"; }
 
 # Check OS type
-os_name=$(grep ^ID= /etc/*release | awk -F'=' '{print $2}' | sed 's/"//g')
-[[ "$os_name" != "centos" && "$os_name" != "rhel" && "$os_name" != "rocky" && "$os_name" != "almalinux" ]] && exit 0
-[ "$(id -u)" -ne "0" ] && exit 1
+os_info=$(grep ^ID= /etc/*release | awk -F'=' '{print $2}' | sed 's/"//g')
+[[ "$os_info" != "centos" && "$os_info" != "rhel" && "$os_info" != "rocky" && "$os_info" != "almalinux" ]] && exit 0
+[ "$(id -u)" -ne "0" ] && _err_msg "$(_red '需要root用户才能运行！')" && exit 1
 
 # Ensure running from root directory
 if [ "$(cd -P -- "$(dirname -- "$0")" && pwd -P)" != "/root" ]; then
@@ -43,15 +44,16 @@ fi
 
 _yellow "Update on 2022.01.14 if there is an error！ @OP"
 
-pro_variable="
-    -e VUE_APP_BASE_URL="weixin.car.com.cn/tg-car-api/mini" \
-    -e VUE_APP_BASE_URL_OTHER="weixin.car.com.cn/tg-car-api" \
-    -e VUE_APP_BASE_URL_OTHER_API="weixin.car.com.cn/tg-car-api" \
-    -e VUE_APP_BASE_URL_DSP="gateway.car.com.cn" \
-"
+pro_variable=(
+    "-e VUE_APP_BASE_URL='weixin.car.com.cn/tg-car-api/mini'"
+    "-e VUE_APP_BASE_URL_OTHER='weixin.car.com.cn/tg-car-api'"
+    "-e VUE_APP_BASE_URL_OTHER_API='weixin.car.com.cn/tg-car-api'"
+    "-e VUE_APP_BASE_URL_DSP='gateway.car.com.cn'"
+)
 
 check_image() {
-    local contrast=$(docker images --format '{{.Repository}}:{{.Tag}}')
+    local contrast
+    contrast=$(docker images --format '{{.Repository}}:{{.Tag}}')
 
     if echo "${contrast}" | grep -q "$param"; then
         _red "Mirror exists！Or Please use \"docker run\" function！" && exit 1
@@ -59,21 +61,22 @@ check_image() {
 }
 
 deploy_image() {
-    local runcon=$(docker ps -q -f "name=${svc_name}")
-    local old_img_log=$(docker ps --format "{{.Image}}" | egrep -i "${svc_name}")
+    local runcon old_img_log
+    runcon=$(docker ps -q -f "name=${svc_name}")
+    old_img_log=$(docker ps --format "{{.Image}}" | grep -Ei "${svc_name}")
 
     _yellow "Beginning to (update|deploy)"
 
-    docker pull ${param} || { _red "Please check your image name and try again！"; exit 1; }
+    docker pull "${param}" || { _red "Please check your image name and try again！"; exit 1; } 
 
     echo "$(date -u -d '+8 hours' +'%Y-%m-%d %H:%M:%S') changed [${old_img_log}]" >> ./live_oldimage.log
 
     # Stop and remove the old container
-    echo "" && docker stop ${runcon} >/dev/null 2>&1 && action "[Stop old service container]"
-    echo "" && docker rm -f ${runcon} >/dev/null 2>&1 && action "[Remove old service container]"
+    echo "" && docker stop "${runcon}" >/dev/null 2>&1 && action "[Stop old service container]"
+    echo "" && docker rm -f "${runcon}" >/dev/null 2>&1 && action "[Remove old service container]"
 
     # Run the new container
-    echo "" && docker run -d --restart=unless-stopped --name=${svc_name}-$RANDOM -p ${host_port}:${container_port} ${pro_variable} ${param} && action "[Deploy new service container]"
+    echo "" && docker run -d --restart=unless-stopped --name="${svc_name}-$RANDOM" -p "${host_port}:${container_port}" "${pro_variable[@]}" "${param}" && action "[Deploy new service container]"
 
     # Wait for the container to be fully started
     _yellow "Please wait 2 seconds" && sleep 2 && docker ps | grep -i "${param}"
