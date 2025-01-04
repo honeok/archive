f187acfe68d5ebcebb07059994f8800063f5315f
honeok <yihaohey@gmail.com>
2025-01-04 15:35:54 +0800

    refactor: modify the existing function definition

--- base.sh
+++ base.sh
@@ -5,10 +5,13 @@
 # Copyright (C) 2000 honeok <honeok@duck.com>
 #
 # https://github.com/honeok/archive/raw/master/base.sh
-
+#
 # shellcheck disable=SC2034,SC2164
 
-script_v='v0.0.0 (2000.01.01)' # major: 重大更新通常会导致向后不兼容的变化 minor: 小功能更新或向后兼容的改动 patch: 小修复和bug修复
+script_v='v0.0.0 (2000.01.01)' 
+# major: 重大更新通常会导致向后不兼容的变化
+# minor: 小功能更新或向后兼容的改动
+# patch: 小修复和bug修复
 
 yellow='\033[93m'
 red='\033[31m'
@@ -27,16 +30,14 @@ _cyan() { echo -e "${cyan}$*${white}"; }
 _purple() { echo -e "${purple}$*${white}"; }
 _gray() { echo -e "${gray}$*${white}"; }
 _orange() { echo -e "${orange}$*${white}"; }
-_white() { echo -e "${white}$*${white}"; }
 
-_info_msg() { echo -e "\033[48;5;220m\033[1m提示${white} $*"; }
+_info_msg() { echo -e "\033[48;5;178m\033[1m\033[97m提示${white} $*"; }
 _err_msg() { echo -e "\033[41m\033[1m警告${white} $*"; }
 _suc_msg() { echo -e "\033[42m\033[1m成功${white} $*"; }
 
 short_separator() { printf "%-20s\n" "-" | sed 's/\s/-/g'; }
 long_separator() { printf "%-40s\n" "-" | sed 's/\s/-/g'; }
 
-export LANG=en_US.UTF-8
 export DEBIAN_FRONTEND=noninteractive
 
 if [ "$(cd -P -- "$(dirname -- "$0")" && pwd -P)" != "/root" ]; then
