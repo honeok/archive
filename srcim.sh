4fbd0c53c17a8d56aca5295675df250807bcb86a
honeok <yihaohey@gmail.com>
2025-01-08 01:36:47 +0800

    update

--- srcim.sh
+++ srcim.sh
@@ -14,5 +14,48 @@
 set \
     -o errexit
 
-script_route=$(realpath "$(dirname "$0")"/$(basename "$0"))
-script_dir=$(dirname $(realpath ${script_route}))
+SCRIPT=$(realpath $(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)/$(basename ${BASH_SOURCE:-$0}))
+SCRIPT_DIR=$(dirname $(realpath ${SCRIPT}))
+
+PROJECT_BASE=$(
+    CURRENT_DIR="${SCRIPT_DIR}"
+    while true; do
+        if [ -d "${CURRENT_DIR}/.git" ] || [ -d "${CURRENT_DIR}/.svn" ] || [ -d "${CURRENT_DIR}/.hg" ] || \
+           [ -f "${CURRENT_DIR}/package.json" ] || [ -f "${CURRENT_DIR}/pom.xml" ] || [ -f "${CURRENT_DIR}/build.gradle" ]; then
+            break
+        fi
+
+        PARENT_DIR=$(dirname "${CURRENT_DIR}")
+        if [ "${CURRENT_DIR}" = "/" ] || [ "${CURRENT_DIR}" = "${PARENT_DIR}" ]; then
+            echo "Error: Could not find project base directory!" >&2
+            exit 1
+        fi
+
+        CURRENT_DIR="${PARENT_DIR}"
+    done
+    echo "${CURRENT_DIR}"
+)
+
+WORK_DIR=$(pwd)
+
+if [[ -z ${PROJECT_BASE} ]]; then
+    PROJECT_BASE=${WORK_DIR}
+fi
+
+if [[ $# -lt 1 ]]; then
+    cat <<EOF
+    usage:
+    build for Java|Nodejs| ... source
+
+    $(realpath $0)   /home/user/project
+
+EOF
+    exit 1
+fi
+
+printf "%-40s\n" "#" | sed 's/\s/#/g'
+echo "PROJECT_BASE=${PROJECT_BASE}"
+env | grep -Ev LS_COLORS
+printf "%-40s\n" "#" | sed 's/\s/#/g'
+echo "Start build"
+GAVE_PROJECT_BASE=$(realpath $1)
