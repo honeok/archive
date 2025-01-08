c2b72ccf241dd0b9defe8a0797ca41532dab2c2a
honeok <yihaohey@gmail.com>
2025-01-08 11:34:09 +0800

    update

--- srcim.sh
+++ srcim.sh
@@ -2,7 +2,9 @@
 #
 # Description: Build Docker image and automatically push it to a container registry.
 #
-# Copyright (C) 2021 - 2025 honeok <honeok@duck.com>
+# Original Author: Jimmy 2021.10 @Next eng
+# Forked and Modified By: 2021 - 2025 honeok <honeok@duck.com>
+#
 # https://www.honeok.com
 # https://github.com/honeok/archive/raw/master/srcim.sh
 #
@@ -16,46 +18,3 @@ set \
 
 SCRIPT=$(realpath $(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)/$(basename ${BASH_SOURCE:-$0}))
 SCRIPT_DIR=$(dirname $(realpath ${SCRIPT}))
-
-PROJECT_BASE=$(
-    CURRENT_DIR="${SCRIPT_DIR}"
-    while true; do
-        if [ -d "${CURRENT_DIR}/.git" ] || [ -d "${CURRENT_DIR}/.svn" ] || [ -d "${CURRENT_DIR}/.hg" ] || \
-           [ -f "${CURRENT_DIR}/package.json" ] || [ -f "${CURRENT_DIR}/pom.xml" ] || [ -f "${CURRENT_DIR}/build.gradle" ]; then
-            break
-        fi
-
-        PARENT_DIR=$(dirname "${CURRENT_DIR}")
-        if [ "${CURRENT_DIR}" = "/" ] || [ "${CURRENT_DIR}" = "${PARENT_DIR}" ]; then
-            echo "Error: Could not find project base directory!" >&2
-            exit 1
-        fi
-
-        CURRENT_DIR="${PARENT_DIR}"
-    done
-    echo "${CURRENT_DIR}"
-)
-
-WORK_DIR=$(pwd)
-
-if [[ -z ${PROJECT_BASE} ]]; then
-    PROJECT_BASE=${WORK_DIR}
-fi
-
-if [[ $# -lt 1 ]]; then
-    cat <<EOF
-Usage:
-    build for Java|Nodejs| ... Source
-
-Example:
-    $(realpath $0) /home/user/project
-EOF
-    exit 1
-fi
-
-printf "%-40s\n" "#" | sed 's/\s/#/g'
-echo "PROJECT_BASE=${PROJECT_BASE}"
-env | grep -Ev LS_COLORS
-printf "%-40s\n" "#" | sed 's/\s/#/g'
-echo "Start build"
-GAVE_PROJECT_BASE=$(realpath $1)
