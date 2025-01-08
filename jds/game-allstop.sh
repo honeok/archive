80d1f2dc10271fb6aab7fdb6bbc7015c10d4200f
honeok <yihaohey@gmail.com>
2025-01-08 14:31:24 +0800

    update

--- jds/game-allstop.sh
+++ jds/game-allstop.sh
@@ -6,34 +6,72 @@
 #
 # https://github.com/honeok/archive/raw/master/jds/game-allstop.sh
 
-version='v0.0.4 (2025.01.01)'
+set \
+    -o errexit \
+    -o nounset
 
-yellow='\033[93m'
-red='\033[31m'
-green='\033[92m'
-cyan='\033[96m'
+version='v0.0.5 (2025.01.08)'
+
+yellow='\033[1;33m'
+red='\033[1;31m'
+green='\033[1;32m'
+cyan='\033[1;36m'
 white='\033[0m'
 _yellow() { echo -e "${yellow}$*${white}"; }
 _red() { echo -e "${red}$*${white}"; }
 _green() { echo -e "${green}$*${white}"; }
 _cyan() { echo -e "${cyan}$*${white}"; }
 
-_info_msg() { echo -e "\033[48;5;220m\033[1m提示${white} $*"; }
 _err_msg() { echo -e "\033[41m\033[1m警告${white} $*"; }
 _suc_msg() { echo -e "\033[42m\033[1m成功${white} $*"; }
 
-server_range=$(find /data/ -maxdepth 1 -type d -name "server*" | sed 's:.*/::' | grep -E '^server[0-9]+$' | sed 's/server//' | sort -n)
-
 clear
-_cyan "当前脚本版本: $version\n"
+_cyan "当前脚本版本: ${version}"
 
 # 操作系统和权限校验
 [ "$(id -ru)" -ne "0" ] && _err_msg "$(_red '需要root用户才能运行！')" && exit 1
 os_name=$(grep ^ID= /etc/*release | awk -F'=' '{print $2}' | sed 's/"//g')
-[[ "$os_name" != "debian" && "$os_name" != "ubuntu" && "$os_name" != "centos" && "$os_name" != "rhel" && "$os_name" != "rocky" && "$os_name" != "almalinux" ]] && { _err_msg "$(_red '当前操作系统不被支持！')" && exit 0 ;}
+[[ "$os_name" != "debian" && "$os_name" != "ubuntu" && "$os_name" != "centos" && "$os_name" != "rhel" && "$os_name" != "rocky" && "$os_name" != "almalinux" ]] && { _err_msg "$(_red '当前操作系统不被支持！')" && exit 1 ;}
 
-allserver_stop() {
-    cd /data/tool || { _err_msg "$(_red '/data/tool路径错误')" && exit 1; }
+project_name="p8_app_server"
+script_workdir="/data/tool"
+
+end_of() {
+    _yellow "按任意键继续"
+    read -n 1 -s -r -p ""
+    echo ""
+    clear
+}
+
+server_runCheck(){
+    local search_server process_Spell
+    local running_servers=() # 初始化数组
+
+    search_server=$(find /data/ -maxdepth 1 -type d -name "server*" | sed 's:.*/::' | grep -E '^server[0-9]+$' | sed 's/server//' | sort -n)
+
+    # 拼接服务器组校验是否正在运行
+    for run_num in $search_server; do
+        process_Spell="/data/server${run_num}/game/${project_name}"
+
+        if pgrep -f "${process_Spell}" >/dev/null 2>&1; then
+            running_servers+=("${run_num}")
+        fi
+    done
+
+    # 检查是否有运行中的服务器
+    if [ ${#running_servers[@]} -eq 0 ]; then
+        _err_msg "$(_red '没有检测到正在运行的 GameServer')"
+        exit 1
+    fi
+
+    # 将运行中的服务器编号输出到server_range
+    server_range=$(printf "%s\n" "${running_servers[@]}" | sort -n)
+}
+
+server_stop() {
+    server_runCheck
+
+    cd "$script_workdir" || { _err_msg "$(_red "${script_workdir}路径错误")" && exit 1; }
     if pgrep -f processcontrol-allserver.sh >/dev/null 2>&1; then
         pkill -9 -f processcontrol-allserver.sh 1>/dev/null
         [ -f "control.txt" ] && : > control.txt
@@ -69,19 +107,23 @@ allserver_stop() {
     done
 
     wait # 等待并行任务
+
     _suc_msg "$(_green '所有game服务器已完成flush和stop操作')"
 }
 
 # 解析命令行参数
 if [ $# -eq 0 ]; then
-    echo -n "$(_info_msg "$(_yellow '当前为停服操作，确认后按任意键继续\040')")"
-    read -n 1 -s -r -p ""
-    allserver_stop
+    _red "当前为停服操作"
+    end_of
+    server_stop
 else
     for arg in "$@"; do
         case $arg in
             -y|y|-Y|Y)
-                allserver_stop
+                server_stop
+                ;;
+            --debug)
+                set -o xtrace
                 ;;
             *)
                 _err_msg "$(_red '无效选项，请重新输入！')"
