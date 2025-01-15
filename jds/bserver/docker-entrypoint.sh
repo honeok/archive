21ae8a4b39516d620b83e0708673291f0cbd7e64
honeok <yihaohey@gmail.com>
2025-01-15 13:43:55 +0800

    update

--- jds/bserver/docker-entrypoint.sh
+++ jds/bserver/docker-entrypoint.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
-luban_dir="/bserver/config/luban"
-template_luban_dir="/opt/bserver/config/luban"
-template_dir="/opt/bserver"
+luban_config_dir="/bserver/config/luban"
+template_luban_config_dir="/opt/config/luban"
 
-if [ -d "$luban_dir" ] && [ -z "$(ls -A "$luban_dir" 2>/dev/null)" ]; then
-    \cp -rf "$template_luban_dir"/* "$luban_dir/"
+if [ -d "$luban_config_dir" ] && [ -z "$(ls -A "$luban_config_dir" 2>/dev/null)" ]; then
+    \cp -rf "$template_luban_config_dir"/* "$luban_config_dir/"
 fi
 
-if [ -d "$template_dir" ]; then
-    rm -rf "$template_dir" 2>/dev/null
+if [ -d "$template_luban_config_dir" ]; then
+    rm -rf "$template_luban_config_dir" 2>/dev/null
 fi
 
 if [ "$#" -eq 0 ]; then
