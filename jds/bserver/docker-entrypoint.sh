4bccd1d1d9b28fb27e7831c862bb5a759a1db88f
honeok <yihaohey@gmail.com>
2025-01-15 19:14:15 +0800

    refactor: improved bserver build process

--- jds/bserver/docker-entrypoint.sh
+++ jds/bserver/docker-entrypoint.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-luban_dir="/bserver/config/luban"
+luban_config_dir="/bserver/config/luban"
+template_luban_config_dir="/opt/config/luban"
 
-if [ -d "$luban_dir" ] && [ -z "$(ls -A "$luban_dir" 2>/dev/null)" ]; then
-    \cp -rf /opt/bserver/config/luban/* "$luban_dir/"
+if [ -d "$luban_config_dir" ] && [ -z "$(ls -A "$luban_config_dir" 2>/dev/null)" ]; then
+    \cp -rf "$template_luban_config_dir"/* "$luban_config_dir/"
 fi
 
-if [ -d "/opt/bserver" ]; then
-    rm -rf /opt/bserver >/dev/null 2>&1
+if [ -d "$template_luban_config_dir" ]; then
+    rm -rf "$template_luban_config_dir" 2>/dev/null
 fi
 
-exec "$@"
+if [ "$#" -eq 0 ]; then
+    exec "/bserver/bserver"
+else
+    exec "$@"
+fi
