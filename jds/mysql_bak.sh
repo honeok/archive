ff031c42958577a95814fbfac2b51c0204323b98
honeok <yihaohey@gmail.com>
2025-01-18 13:46:52 +0800

    chore: Define game database array and streamline variable definitions

--- jds/mysql_bak.sh
+++ jds/mysql_bak.sh
@@ -43,13 +43,13 @@ _cyan "当前脚本版本: ${version}\n"
 
 # https://github.com/koalaman/shellcheck/wiki/SC2155
 os_name=$(grep "^ID=" /etc/*release | awk -F'=' '{print $2}' | sed 's/"//g')
-timeStamp=$(date -u -d '+8 hours' +"%Y-%m-%d_%H-%M-%S")
+timeStamp=$(date -u -d '+8 hours' +"%Y-%m-%d_%H:%M:%S") # 使用 UTC+8 时间戳，保证SQL文件为东八区
 mysql_bak_pid='/tmp/mysql_bak.pid'
 bakDir='/data/dbbak'
 mysql_host='10.46.96.179'
-mysql_password="$(awk 'NR==1 {gsub(/^[ \t]+|[ \t]+$/, ""); print}' "$HOME/mysql_password.txt" 2>/dev/null)"
+mysql_password=$(awk 'NR==1 {gsub(/^[ \t]+|[ \t]+$/, ""); print}' "$HOME/mysql_password.txt" 2>/dev/null)
 mysqldump_cmd="$(which mysqldump 2>/dev/null)"
-readonly os_name timeStamp bakDir mysql_host mysqldump_cmd
+readonly os_name mysql_bak_pid bakDir mysql_host mysql_password mysqldump_cmd
 
 # 声明全局数组，确保跨函数使用
 declare -a old_sql
@@ -89,16 +89,15 @@ delete_oldsql() {
         return
     fi
 
-    # 遍历索引取值
-    for sql_file in "${old_sql[@]}"; do
-        rm -f "$sql_file"
-        _suc_msg "$(_green "已删除旧SQL文件: $sql_file")"
+    for old_sql_file in "${old_sql[@]}"; do
+        rm -f "$old_sql_file"
+        _suc_msg "$(_green "已删除SQL文件: $old_sql_file")"
     done
 }
 
 backup_sql() {
     if [ -z "$mysql_password" ] || [ -z "$mysqldump_cmd" ]; then
-        _err_msg "$(_red 'MySQL密码和mysqldump命令不能为空！请检查配置和安装情况')"
+        _err_msg "$(_red 'mysql密码和mysqldump命令不能为空！请检查配置和安装情况')"
         exit 1
     fi
 
@@ -107,11 +106,15 @@ backup_sql() {
     local db_password="$mysql_password"
     local db_host="$mysql_host"
     local db_port="3306"
+    local mysqldump_options
+
+    mysqldump_options="--no-defaults --single-transaction --set-gtid-purged=OFF"
 
     # --no-defaults: 忽略默认的配置文件，希望 mysqldump 命令不受系统默认配置文件中的设置影响，或者你担心默认配置文件中的某些设置会干扰备份过程（例如，某些连接设置或插件），可以使用该选项
     # --single-transaction: 用于在备份过程中保持数据库的一致性，mysqldump 会在开始时创建一个事务，然后在整个备份过程中保持这个事务，以确保数据的一致性。它适用于支持事务的存储引擎（如 InnoDB）
     # --set-gtid-purged=OFF: 控制 GTID（全局事务标识符）的导出。GTID 用于 MySQL 的复制功能，它确保每个事务有一个唯一的标识符，以便于复制和故障恢复
 
+    declare -a game_db
     declare -a special_db
 
     if [ ! -d "$bakDir" ]; then
@@ -122,12 +125,12 @@ backup_sql() {
     fi
 
     # 游戏数据库
-    for game_db_num in $(seq 1 7); do
-        if "$mysqldump_cmd" --no-defaults --single-transaction --set-gtid-purged=OFF \
-            -h "$db_host" -P "$db_port" -u "$db_user" -p"$db_password" -R "cbt4_game_$game_db_num" > "$bakDir/cbt4_game_${game_db_num}_$timeStamp.sql"; then
-            _suc_msg "$(_green "备份 cbt4_game_${game_db_num} 完成")"
+    game_db=( cbt4_game_1 cbt4_game_2 cbt4_game_3 cbt4_game_4 cbt4_game_5 cbt4_game_6 cbt4_game_7 )
+    for game_db_name in "${game_db[@]}"; do
+        if "$mysqldump_cmd" "$mysqldump_options" -h "$db_host" -P "$db_port" -u "$db_user" -p"$db_password" -R "$game_db_name" > "$bakDir/${game_db_name}_$timeStamp.sql"; then
+            _suc_msg "$(_green "备份 ${game_db_name} 完成")"
         else
-            _err_msg "$(_red "备份 cbt4_game_${game_db_num} 失败，暂时跳过！")"
+            _err_msg "$(_red "备份 ${game_db_name} 失败，暂时跳过！")"
             continue
         fi
     done
@@ -135,8 +138,7 @@ backup_sql() {
     # 其余数据库单独处理
     special_db=( cbt4_common cbt4_account cbt4_center cbt4_report cbt4_gm )
     for special_db_name in "${special_db[@]}"; do
-        if "$mysqldump_cmd" --no-defaults --single-transaction --set-gtid-purged=OFF \
-            -h "$db_host" -P "$db_port" -u "$db_user" -p"$db_password" -R "$special_db_name" > "$bakDir/${special_db_name}_$timeStamp.sql"; then
+        if "$mysqldump_cmd" "$mysqldump_options" -h "$db_host" -P "$db_port" -u "$db_user" -p"$db_password" -R "$special_db_name" > "$bakDir/${special_db_name}_$timeStamp.sql"; then
             _suc_msg "$(_green "备份 ${special_db_name} 完成")"
         else
             _err_msg "$(_red "备份 ${special_db_name} 失败，暂时跳过！")"
