5dd55d2a16f9f214230feb09e714643ce4306d8f
honeok <yihaohey@gmail.com>
2025-02-01 15:47:41 +0800

    chore: add description

--- jds/p8/gameapi/base/build_dep.sh
+++ jds/p8/gameapi/base/build_dep.sh
@@ -1,15 +1,31 @@
 #!/bin/sh
 #
-# Description: Configure dependencies for the gameapi Docker image.
-# System Required: alpine3.19+
+# Description: Configure dependencies for the gameapi Docker image on Alpine Linux.
+# System Required: Alpine 3.19 or higher.
 #
 # Copyright (C) 2025 honeok <honeok@duck.com>
-# https://www.honeok.com
-# https://github.com/honeok/archive/raw/master/jds/game-api/base/build_dep.sh
+#
+# https://github.com/honeok/archive/raw/master/jds/p8/game-api/base/build_dep.sh
+#      __     __       _____                  
+#  __ / / ___/ /  ___ / ___/ ___ _  __ _  ___ 
+# / // / / _  /  (_-</ (_ / / _ `/ /  ' \/ -_)
+# \___/  \_,_/  /___/\___/  \_,_/ /_/_/_/\__/ 
+#                                             
+# License Information:
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License, version 3 or later.
+#
+# This program is distributed WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <https://www.gnu.org/licenses/>.
 
 set \
     -o errexit \
     -o nounset \
+    -o pipefail \
     -o xtrace
 
 os_name=$(grep ^ID= /etc/*release | awk -F'=' '{print $2}' | sed 's/"//g')
