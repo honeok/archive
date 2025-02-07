e436dd29dda7383f54fe35700ba7b68d98473050
honeok <yihaohey@gmail.com>
2025-02-07 17:22:24 +0800

    chore: rename and add comments for declarations

--- jds/gmtools/gm-server/build.sh
+++ jds/gmtools/gm-server/build.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 #
 # Description: Configure dependencies for the gm-server Docker image.
-# System Required: alpine3.18+
 #
 # Copyright (C) 2025 honeok <honeok@duck.com>
-# https://www.honeok.com
-# https://github.com/honeok/archive/raw/master/jds/gmtools/gm-server/build_dep.sh
+#
+# License Information:
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License, version 3 or later.
+#
+# This program is distributed WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <https://www.gnu.org/licenses/>.
 
 set \
     -o errexit \
