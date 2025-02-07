b70f15a9625b816cbd387a95787a6b3b6b483187
honeok <yihaohey@gmail.com>
2025-02-07 20:12:03 +0800

    fix: simplify and optimize startup logic

--- jds/gmtools/gm-server/docker-entrypoint.sh
+++ jds/gmtools/gm-server/docker-entrypoint.sh
@@ -7,21 +7,14 @@ set \
 work_dir="/gm-server"
 
 if [ -d "$work_dir" ]; then
-    cd "$work_dir" || { echo "Failed to enter work path!" && exit 1; }
-    if [ ! -f config/gamedb.js ]; then
-        echo "error: You need to first mount gamedb.js file!"
-        exit 1
-    fi
-    if [ ! -f config/setup.js ]; then
-        envsubst < templates/setup.js.template > config/setup.js
-    fi
-    if [ ! -f config/obs.js ]; then
-        envsubst < templates/obs.js.template > config/obs.js
-    fi
+    cd "$work_dir" || { echo "error: Failed to enter work path!" && exit 1; }
 else
-    echo "Directory $work_dir does not exist, exiting!"
-    exit 1
+    echo "error: Directory $work_dir does not exist, exiting!" && exit 1
 fi
+
+[ ! -f "config/gamedb.js" ] && { echo "error: You need to first mount gamedb.js file!" && exit 1; }
+[ ! -f "config/setup.js" ] && envsubst < templates/setup.js.template > config/setup.js
+[ ! -f "config/obs.js" ] && envsubst < templates/obs.js.template > config/obs.js
 
 if [ "$#" -eq 0 ]; then
     exec npm --prefix "$work_dir/" start
