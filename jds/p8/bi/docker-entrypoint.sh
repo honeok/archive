92949ad42fd14d13952425cc8b33b3dfe1efcd36
honeok <yihaohey@gmail.com>
2025-02-08 18:19:42 +0800

    fix: simplify and optimize startup logic

--- jds/p8/bi/docker-entrypoint.sh
+++ jds/p8/bi/docker-entrypoint.sh
@@ -1,18 +1,11 @@
 #!/bin/sh
 
-set \
-    -o errexit \
-    -o nounset
+set -eu
 
 work_dir="/bi"
 
 if [ -z "$DB_USER" ] || [ -z "$DB_PASSWORD" ] || [ -z "$DB_HOST" ] || [ -z "$DB_PORT" ] || [ -z "$DB_DATABASE" ]; then
     echo "error: required environment variables are missing!"
-    exit 1
-fi
-
-if ! mysql -u "$DB_USER" -P "$DB_PORT" -h "$DB_HOST" -p"$DB_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $DB_DATABASE CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;" 2>/dev/null; then
-    echo "error: Failed to create the database $DB_DATABASE. Exiting!"
     exit 1
 fi
 
@@ -24,6 +17,16 @@ fi
 
 [ ! -f ".env" ] && envsubst < templates/.env.template > .env
 [ ! -f "aerich_env.py" ] && envsubst < templates/aerich_env.py.template > aerich_env.py
+
+set +e
+echo "initialize the database"
+
+if ! python manager.py initdb >/dev/null 2>&1; then
+    echo "database exists, skip execution!"
+else
+    echo "initialize the database"
+fi
+set -e
 
 if command -v aerich >/dev/null 2>&1; then
     aerich init -t aerich_env.TORTOISE_ORM
