f6a5c0d48ab1a7010ab4f1c37a4930a2ab0c5d5f
honeok <yihaohey@gmail.com>
2025-02-20 17:06:19 +0800

    chore: remove create of runtime dir

--- jds/p8/gmapi/build.sh
+++ jds/p8/gmapi/build.sh
@@ -13,12 +13,3 @@ WORK_DIR="/gmapi"
 RUN_DIR="$WORK_DIR/run"
 
 [ -d "${RUN_DIR:?}" ] && find "$RUN_DIR" -mindepth 1 -delete
-
-# 运行环境准备
-mkdir -p \
-    "$RUN_DIR/logs" \
-    "$RUN_DIR/temp/client-body" \
-    "$RUN_DIR/temp/proxy" \
-    "$RUN_DIR/temp/fastcgi" \
-    "$RUN_DIR/temp/uwsgi" \
-    "$RUN_DIR/temp/scgi"
