07abb696c8e8dc82ac25a3cebd3c97e86f16e09a
honeok <yihaohey@gmail.com>
2025-04-03 17:45:11 +0800

    feat: add update and maintenance playbooks for additional components

--- jds/p8/ansible/update/update.sh
+++ jds/p8/ansible/update/update.sh
@@ -1,71 +1,129 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# vim:sw=4:ts=4:et
+#
+# Description: Ansible playbook launcher with automatic adaptation for various scenarios.
+#
+# Copyright (c) 2025 honeok <honeok@duck.com>
+#
+# Thanks: zzwsec <zzwsec@163.com>
+#
+# Licensed under the MIT License.
+# This software is provided "as is", without any warranty.
 
+export DEBIAN_FRONTEND=noninteractive
+
+red='\033[91m'
+green='\033[92m'
+yellow='\033[93m'
+white='\033[0m'
+_red() { echo -e "${red}$*${white}"; }
+_green() { echo -e "${green}$*${white}"; }
+_yellow() { echo -e "${yellow}$*${white}"; }
+
+_err_msg() { echo -e "\033[41m\033[1mError${white} $*"; }
+_suc_msg() { echo -e "\033[42m\033[1mSuccess${white} $*"; }
+
+# 各变量默认值
 WORK_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SHARE_DIR="$WORK_DIR/share"
 
-err_exit() {
-    echo "$1" >&2
-    exit "$2"
-}
-
-print_info_and_execute_playbook() {
-    local option="$1"
-    if [ "$option" = "group" ]; then
-        echo "检测到 groups.lua 执行更新 group.lua 操作，按任意键继续..."
-        read -r || true  # 防止无输入时出错
-        update_group_lua
-    elif [ "$option" = "increment" ]; then
-        echo "检测到 increment.tar.gz 执行更新操作，按任意键继续..."
-        read -r || true
-        update_increment
+_exists() {
+    local _cmd="$1"
+    if type "$_cmd" >/dev/null 2>&1; then
+        return 0
+    elif command -v "$_cmd" >/dev/null 2>&1; then
+        return 0
     else
-        err_exit "异常值: $option" 3  # 更详细的错误信息
+        return 1
     fi
 }
 
-update_option() {
-    local playbook_path="$1"
-    local tag="$2"
+pkg_install() {
+    for package in "$@"; do
+        _yellow "Installing $package"
+        if _exists 'dnf'; then
+            dnf install -y "$package"
+        elif _exists 'yum'; then
+            yum install -y "$package"
+        elif _exists 'apt-get'; then
+            apt-get install -y -q "$package"
+        elif _exists 'apt'; then
+            apt install -y -q "$package"
+        elif _exists 'pacman'; then
+            pacman -S --noconfirm --needed "$package"
+        elif _exists 'zypper'; then
+            zypper install -y "$package"
+        fi
+    done
+}
 
-    [ ! -f "$playbook_path" ] && err_exit "playbook 文件 $playbook_path 不存在" 1
-    if [ -n "$tag" ]; then
-        ansible-playbook "$playbook_path" -t "$tag" || err_exit "Ansible 执行失败: $playbook_path" 4
-    else
-        ansible-playbook "$playbook_path" || err_exit "Ansible 执行失败: $playbook_path" 4
+pre_check() {
+    local depend_pkg
+    depend_pkg=( 'ansible' 'ansible-playbook' )
+
+    # 检测ansible安装
+    for pkg in "${depend_pkg[@]}"; do
+        if ! _exists "$pkg" >/dev/null 2>&1; then
+            pkg_install "$pkg"
+        fi
+    done
+    # 如果没有共享文件夹认为是第一次使用则执行创建
+    if [ ! -d "$SHARE_DIR" ]; then
+        mkdir -p "$SHARE_DIR" >/dev/null 2>&1
+        _suc_msg "$(_green "First use? $SHARE_DIR created.")" && exit 0
     fi
 }
 
-update_group_lua() {
-    update_option "playbook/cross/cross-entry.yaml" "groups"
-    update_option "playbook/game/game-entry.yaml" "groups"
+before_event() {
+    if [ -f "$SHARE_DIR/groups.lua" ]; then
+        exec_event='groups'
+    elif [ -f "$SHARE_DIR/increment.tar.gz" ]; then
+        exec_event='increment'
+    elif [ -f "$SHARE_DIR/updategame.tar.gz" ]; then
+        exec_event='maint'
+    else
+        _err_msg "$(_red 'List of tasks with no matches.')" && exit 1
+    fi
 }
 
-update_increment() {
-    update_option "playbook/cross/cross-entry.yaml" "increment"
-    update_option "playbook/game/game-entry.yaml" "increment"
-    update_option "playbook/gm/gm-entry.yaml" ""
-    update_option "playbook/log/log-entry.yaml" ""
+exec_playbook() {
+    case "$exec_event" in
+        'groups')
+            ansible-playbook game.yml --tags 'groups'
+            ansible-playbook cross.yml --tags 'groups'
+        ;;
+        'increment')
+            ansible-playbook game.yml --tags 'increment'
+            ansible-playbook cross.yml --tags 'increment'
+            ansible-playbook log.yml --tags 'increment'
+            ansible-playbook gm.yml --tags 'increment'
+        ;;
+        'maint')
+            ansible-playbook game.yml --tags 'maint'
+            ansible-playbook cross.yml --tags 'maint'
+            ansible-playbook log.yml --tags 'maint'
+            ansible-playbook gm.yml --tags 'maint'
+            ansible-playbook global.yml
+            ansible-playbook zk.yml
+        ;;
+        *)
+            _err_msg "$(_red 'List of tasks with no matches.')" && exit 1
+        ;;
+    esac
 }
 
-# 检查 ./file/ 目录是否存在
-[ ! -d ./file/ ] && err_exit "错误: 目录 ./file/ 不存在" 1
+after_event() {
+    if [ -n "$WORK_DIR" ] && [ -n "$(ls -A "$SHARE_DIR")" ]; then
+        rm -rf "${SHARE_DIR:?}/"* 2>/dev/null
+    fi
+    _green 'Job Success' ; exit 0
+}
 
-# 检查ansible是否安装
-command -v ansible >/dev/null 2>&1 || err_exit "错误: ansible未安装" 1
+update() {
+    pre_check
+    before_event
+    exec_playbook
+    after_event
+}
 
-# 统计文件数量
-group_stat=$(find "$WORK_DIR/file" -name "groups.lua" -type f | wc -l)
-increment_stat=$(find "$WORK_DIR/file" -name "increment.tar.gz" -type f | wc -l)
-
-# 根据文件存在情况执行相应操作
-if [ "$group_stat" -eq 1 ] && [ "$increment_stat" -eq 0 ]; then
-    print_info_and_execute_playbook "group"
-elif [ "$group_stat" -eq 0 ] &&  [ "$increment_stat" -eq 1 ]; then
-    tar tf "$WORK_DIR/file/increment.tar.gz" | sed -n '1p' | grep -q "app/" || err_exit "increment.tar.gz 未包含 app 目录" 2
-    print_info_and_execute_playbook "increment"
-elif [ "$group_stat" -eq 1 ] && [ "$increment_stat" -eq 1 ]; then
-    err_exit "groups.lua 和 increment.tar.gz 同时存在，请删除或移动其中一个" 2
-else
-    err_exit "groups.lua 或 increment.tar.gz 不存在，请检查 file 目录" 2
-fi
-
-rm -rf "$WORK_DIR/file/"*
+update
